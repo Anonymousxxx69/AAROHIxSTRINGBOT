46d7c47132de45b25e0463a842f591ba606d63c2
Anonymousxxx69 <spiderman19374638@gmail.com>
2025-03-22 20:42:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","14050586"))
-API_HASH = getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
+API_ID = int(getenv("API_ID","14800186"))
+API_HASH = getenv("API_HASH","7b1f5b8e41eaaa29b8f4e5c4aecce7bf")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://shivamyadav:shivam.yadav_23@cluster0.0awjqhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("MUST_JOIN", https://t.me/Unknown_RKP1)
